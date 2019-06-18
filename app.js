473cbee7003904b6066c0380fd143cbf586286c0
Abby Gottlich <abby.gottlich@gmail.com>
2019-06-18 08:47:45 -0500

    experimenting with different types

--- app.js
+++ app.js
@@ -1,2 +1,33 @@
 "use strict";
-console.log("It works!");
+// string
+var myName = "Max";
+// myName = 28;
+// number
+var myAge = 27;
+// myAge = "hello";
+// boolean
+var hasHobbies = false;
+// hasHobbies = 1;
+// assign types
+var myRealAge;
+myRealAge = 25;
+// myRealAge = "hi";
+// array
+var hobbies = ["cooking", "sports"];
+hobbies = [100];
+// tuples
+var address = ["Superstreet", 99];
+// enum
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Green"] = 100] = "Green";
+    Color[Color["Blue"] = 101] = "Blue";
+})(Color || (Color = {}));
+var myColor = Color.Blue;
+// console.log(myColor);
+// any
+var car = "BMW";
+console.log(car);
+car = { brand: "BMW", series: 3 };
+console.log(car);
--- app.ts
+++ app.ts
@@ -1,1 +1,38 @@
-console.log("It works!");
+// string
+let myName = "Max";
+// myName = 28;
+
+// number
+let myAge = 27;
+// myAge = "hello";
+
+// boolean
+let hasHobbies = false;
+// hasHobbies = 1;
+
+// assign types
+let myRealAge: number;
+myRealAge = 25;
+// myRealAge = "hi";
+
+// array
+let hobbies: any[] = ["cooking", "sports"];
+hobbies = [100];
+
+// tuples
+let address: [string, number] = ["Superstreet", 99];
+
+// enum
+enum Color {
+    Gray,
+    Green = 100,
+    Blue
+}
+let myColor: Color = Color.Blue;
+// console.log(myColor);
+
+// any
+let car: any = "BMW";
+console.log(car);
+car = {brand: "BMW", series: 3};
+console.log(car);
