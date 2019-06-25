65a12f0f6d4768601ecaa45b7a10c97976bbb3b8
Abby Gottlich <abby.gottlich@gmail.com>
2019-06-25 08:50:15 -0500

    class methods practice

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 var Person = /** @class */ (function () {
     function Person(name, username) {
         this.username = username;
+        this.age = 27;
         this.name = name;
     }
+    Person.prototype.printAge = function () {
+        console.log(this.age);
+        this.setType("Old guy");
+    };
+    Person.prototype.setType = function (type) {
+        this.type = type;
+        console.log(this.type);
+    };
     return Person;
 }());
 var person = new Person("Max", "max");
 console.log(person.name, person.username);
+person.printAge();
+// person.setType("Cool guy"); //won't work with private method
--- app.ts
+++ app.ts
@@ -1,13 +1,25 @@
 class Person {
     name: string;
     private type: string;
-    protected age: number;
+    protected age: number = 27;
 
     constructor(name: string, public username: string){
         this.name = name;
+    }
+
+    printAge() {
+        console.log(this.age);
+        this.setType("Old guy");
+    }
+
+    private setType(type: string) {
+        this.type = type;
+        console.log(this.type);
     }
 }
 
 const person = new Person("Max", "max");
 console.log(person.name, person.username);
+person.printAge();
+// person.setType("Cool guy"); //won't work with private method
 
