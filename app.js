5bdaab68592804c8a1e77998a8b165abc90ae8b1
Abby Gottlich <abby.gottlich@gmail.com>
2019-06-26 08:44:42 -0500

    inheritance practice

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Person = /** @class */ (function () {
     function Person(name, username) {
         this.username = username;
@@ -18,3 +31,16 @@ var person = new Person("Max", "max");
 console.log(person.name, person.username);
 person.printAge();
 // person.setType("Cool guy"); //won't work with private method
+// Inheritance
+var Max = /** @class */ (function (_super) {
+    __extends(Max, _super);
+    // name = "Max";
+    function Max(username) {
+        var _this = _super.call(this, "Max", username) || this;
+        _this.age = 31;
+        return _this;
+    }
+    return Max;
+}(Person));
+var max = new Max("max");
+console.log(max);
--- app.ts
+++ app.ts
@@ -23,3 +23,15 @@ console.log(person.name, person.username);
 person.printAge();
 // person.setType("Cool guy"); //won't work with private method
 
+// Inheritance
+class Max extends Person {
+    // name = "Max";
+    constructor(username: string) {
+        super("Max", username);
+        this.age = 31;
+    }
+}
+
+const max = new Max("max");
+console.log(max);
+
